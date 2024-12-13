42aa9a6b82164c15010ae893694101c68cc9eb0c
Tomas Maceira <maceiratomas@gmail.com>
2024-12-13 12:48:55 +0100

    Fast push

---
+++ include/functions.php
@@ -0,0 +1,16 @@
+<?php
+function getSiteUrl() {
+    if (
+        isset($_SERVER['HTTPS']) &&
+        ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'
+    ) {
+        $protocol = 'https://';
+    }
+    else {
+        $protocol = 'http://';
+    }
+
+    return $protocol.$_SERVER['HTTP_HOST'].'/';
+}
