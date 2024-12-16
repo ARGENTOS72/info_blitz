7278756803d00894f8b5aef4078c07d821bc933e
Tomas Maceira <maceiratomas@gmail.com>
2024-12-16 18:17:51 +0100

    Added navbar and fixes
    Added an admin navbar with its styling, fixed some subtle bugs

--- templates/admin/utente/create.php
+++ templates/admin/utente/create.php
@@ -7,6 +7,8 @@ if (!isset($_SESSION['login'])) {
     die();
 }
 
+$_SESSION['current_page'] = "utente";
+
 $ruolo = "admin";
 
 if (isset($_POST['create'])) {
@@ -20,11 +22,17 @@ if (isset($_POST['create'])) {
 
     $sql = "INSERT INTO utente (id, nome, cognome, login, password) VALUES
         (null, '$nome', '$cognome', '$login', '$password')";
-    $conn->query($sql);
 
-    header("Location: index.php");
+    try {
+        $result = $conn->query($sql);
 
-    die();
+        header("Location: index.php");
+    
+        die();
+    } catch (mysqli_sql_exception $err) {
+        echo $err->getMessage();
+        echo $err->getSqlState();
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -38,11 +46,11 @@ if (isset($_POST['create'])) {
 <body>
     <?php
     if ($ruolo == "admin") {
-        require "../../helpers/admin_navbar.php";
+    require "../../helpers/admin_navbar.php";
     }
     ?>
+    <div class="container my-4">
     <h1>Creazione utente</h1>
-    <a href="index.php"><- Utenti</a>
     <form method="post">
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome">
@@ -58,6 +66,26 @@ if (isset($_POST['create'])) {
         <br>
         <input type="submit" value="Crea" name="create">
     </form>
+    </div>
+    <script>
+        const form = document.getElementsByTagName('form')[0];
+        let formModified = false;
+
+        form.addEventListener('change', () => {
+            formModified = true;
+        });
+
+        form.addEventListener('submit', () => {
+            formModified = false;
+        });
+
+        window.addEventListener("beforeunload", event => {
+            if (formModified) {
+                event.preventDefault();
+                event.returnValue = "";
+            }
+        });
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
