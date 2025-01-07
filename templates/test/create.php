a506051cc6f4410455dbf12532f39e565e6eba1a
T1r4m1su <117399429+T1r4m1su@users.noreply.github.com>
2025-01-07 17:36:03 +0100

    Update create.php

--- templates/test/create.php
+++ templates/test/create.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['login'])) {
 $_SESSION['current_page'] = "test";
 
 $ruolo = "prof";
+$ruolo = "admin";
 
 if (isset($_POST['create'])) {
     require "../../../include/db.php";
@@ -48,4 +49,36 @@ if (isset($_POST['create'])) {
     }
     ?>
     <div class="container my-4">
-    <h1>Creazione utente</h1>
+    <h1>Creazione utente</h1>    <h1>Creazione utente</h1>
+    <form method="post">
+        <input type="text" id="nome" name="nome">
+        <br>
+        <label for="cognome">Descrizione:</label>
+        <input type="text" id="cognome" name="cognome">
+        <br>
+        
+        <input type="submit" value="Crea" name="create">
+    </form>
+    </div>
+    <script>
+        const form = document.getElementsByTagName('form')[0];
+        let formModified = false;
+
+        form.addEventListener('change', () => {
+            formModified = true;
+        });
+
+        form.addEventListener('submit', () => {
+            formModified = false;
+        });
+
+        window.addEventListener("beforeunload", event => {
+            if (formModified) {
+                event.preventDefault();
+                event.returnValue = "";
+            }
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
