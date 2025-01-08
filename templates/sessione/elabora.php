ac38588fbaa479555bd92840257b955fffbfbad7
Tomas Maceira <maceiratomas@gmail.com>
2025-01-08 08:02:00 +0100

    checkbox

--- templates/sessione/elabora.php
+++ templates/sessione/elabora.php
@@ -12,6 +12,10 @@ if (isset($_POST['invia'])) {
     $conn = accediDb();
 
     foreach ($_POST as $key => $value) {
+        if (is_array($value)) {
+            $value = implode(",", $value);
+        }
+
         if ($key != "invia") {
             $sql = "INSERT INTO risposta (id, id_domanda, risposta) VALUES (null, $key, '$value');";
             $result = $conn->query($sql);
